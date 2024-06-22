939381fb8184695c308c4fabe557a289e0196313
solomonrazak <solomonrazak99@gmail.com>
2024-06-22 08:43:23 -0700

    housing-location component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `<main>
+    <header>
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"/>
+    </header>
+    <section>
+      <app-home></app-home>
+    </section>
+  </main>`,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent]
 })
 export class AppComponent {
   title = 'homes';
