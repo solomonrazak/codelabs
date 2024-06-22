8650e0fc1f6fb97d6573bc1e129a6eadafdb79f0
solomonrazak <solomonrazak99@gmail.com>
2024-06-22 10:20:01 -0700

    home location added

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HousingLocation } from "../housinglocation";
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-housing-location',
+  selector: "app-housing-location",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="HousingLocation.photo"
+        alt="Exterior photo of {{ HousingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ HousingLocation.name }}</h2>
+      <p class="listing-location">{{ HousingLocation.city }}, {{HousingLocation.state}}</p>
+    </section>
   `,
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
-
+  @Input() HousingLocation!: HousingLocation;
 }
