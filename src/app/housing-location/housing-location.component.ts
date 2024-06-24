b94eafbcbf35ad192e00e9d38768590951956752
solomonrazak <solomonrazak99@gmail.com>
2024-06-24 09:56:59 -0700

    services created

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { HousingLocation } from "../housinglocation";
 import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-housing-location",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img
@@ -15,6 +16,7 @@ import { CommonModule } from "@angular/common";
       />
       <h2 class="listing-heading">{{ HousingLocation.name }}</h2>
       <p class="listing-location">{{ HousingLocation.city }}, {{HousingLocation.state}}</p>
+      <a [routerLink]="['/details', HousingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ["./housing-location.component.css"],
